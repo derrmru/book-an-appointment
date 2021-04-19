24d0a6d9001a9b02ebc519cbe77b56a2ec7dc12e
derrmru <podogomacbookair@MacBook-Air.local>
2021-04-19 10:56:11 +0200

    fu patient search url change

--- src/components/FuFinder/FuFinder.tsx
+++ src/components/FuFinder/FuFinder.tsx
@@ -27,9 +27,11 @@ const FuFinder: React.FC<Props> = (props) => {
         e.preventDefault()
         setFound(false)
         setLoad(true)
-        $.post("https://script.google.com/macros/s/AKfycbw1N-hxteK4WXKzXBDtDHeUxNB3LaF47vAES7nnPBNCsqWtG9HxWDvL/exec", 
+        $.post("https://script.google.com/macros/s/AKfycbz-aYMLkSrTD7jHLI_UySYKZyrpGnDkRtpZJ7bhdLAdtazPBts/exec", 
             details, 
-            async (res) => {
+            async (res, status) => {
+                console.log(res)
+                console.log(status)
                 let result = await JSON.parse(res);
                 console.log(result)
                 setLoad(false)
